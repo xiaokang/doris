9c09e35414a82892c8ef9aad69752ee8b85fe11d
qiye <jianliang5669@gmail.com>
2023-04-26 20:52:50 +0800

    [opt](compaction) add pick rowset to compact interval config

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -955,6 +955,8 @@ CONF_Int32(max_depth_of_expr_tree, "600");
 // Report a tablet as bad when io errors occurs more than this value.
 CONF_mInt64(max_tablet_io_errors, "-1");
 
+CONF_Int64(pick_rowset_to_compact_interval, "600");
+
 // Page size of row column, default 4KB
 CONF_mInt64(row_column_page_size, "4096");
 
