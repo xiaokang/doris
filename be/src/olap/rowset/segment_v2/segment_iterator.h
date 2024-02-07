43ae84eefc97b9ca05a3acf0a5182ebca1eb6c4f
Kang <kxiao.tiger@gmail.com>
2024-02-07 21:48:58 +0800

    [fix](index) disable OR predicate for bitmap index (#30971)

--- be/src/olap/rowset/segment_v2/segment_iterator.h
+++ be/src/olap/rowset/segment_v2/segment_iterator.h
@@ -183,8 +183,6 @@ private:
             std::set<const ColumnPredicate*>& no_need_to_pass_column_predicate_set,
             bool* continue_apply);
     [[nodiscard]] Status _apply_index_except_leafnode_of_andnode();
-    [[nodiscard]] Status _apply_bitmap_index_except_leafnode_of_andnode(
-            ColumnPredicate* pred, roaring::Roaring* output_result);
     [[nodiscard]] Status _apply_inverted_index_except_leafnode_of_andnode(
             ColumnPredicate* pred, roaring::Roaring* output_result);
     bool _column_has_fulltext_index(int32_t unique_id);
@@ -252,7 +250,6 @@ private:
 
     void _update_max_row(const vectorized::Block* block);
 
-    bool _check_apply_by_bitmap_index(ColumnPredicate* pred);
     bool _check_apply_by_inverted_index(ColumnPredicate* pred, bool pred_in_compound = false);
 
     std::string _gen_predicate_result_sign(ColumnPredicate* predicate);
